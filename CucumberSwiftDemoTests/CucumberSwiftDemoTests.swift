99aa9f9f03a182485930e7f18d4d137f3bb490f9
Eyal Yovel <eyalyovel@gmail.com>
2025-05-29 14:45:48 +0100

    Initial Commit

---
+++ CucumberSwiftDemoTests/CucumberSwiftDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CucumberSwiftDemoTests.swift
+//  CucumberSwiftDemoTests
+//
+//  Created by eyalyovel on 29/05/2025.
+//
+
+import Testing
+@testable import CucumberSwiftDemo
+
+struct CucumberSwiftDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
